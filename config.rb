a0f8d431b08e6f4134733c691dcd527c2835da9b
J Irving <j@lollyshouse.net>
2012-04-30 20:16:46 -0400

    Updates for new URLs.

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/diazepam/"
+http_images_path = "/diazepam/images"
+http_fonts_path = "/diazepam/fonts"
+css_dir = "public/diazepam/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
